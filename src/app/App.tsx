dbd4b886839bbb9c08edd46b72e4bc7c0d3c5944
shrvse <sergeq2222@gmail.com>
2026-02-27 00:08:00 +0500

    +

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,16 @@
 import { Providers } from './providers';
 import { AppRouter } from './router';
+import styles from './App.module.css';
 
 export function App() {
+  const isFirefox =
+    typeof navigator !== 'undefined' && /firefox|fxios/i.test(navigator.userAgent);
+
   return (
     <Providers>
+      {isFirefox ? (
+        <div className={styles.browserWarning}>Воспользуйтесь браузером google chrome</div>
+      ) : null}
       <AppRouter />
     </Providers>
   );
